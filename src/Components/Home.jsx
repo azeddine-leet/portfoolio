036242b1deb562a2c00a60c408d683a246915bd0
Azeddine El makkaoui <ael-makk@e3r3p2.1337.ma>
2023-08-21 03:48:36 +0100

    Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,13 @@
-
+import Avatar from "./../assets/avatar.png"
 export default function Home() {
     return (
         <>
-
-            <h1 className=''>Home</h1>
+            <img 
+                src={ Avatar }
+                alt="avatar"
+                className="rounded-full w-64 h-64 border border-black bg-black"
+            />
+            <h1 className="">Azeddine Elmakkaoui</h1>
         </>
     );
 }
