0a4e628e886e217878282738d8c58c6256eaa827
Azeddine El makkaoui <ael-makk@e3r4p7.1337.ma>
2023-08-21 16:06:08 +0100

    home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,17 @@
 import Avatar from "./../assets/avatar.png"
+
+
 export default function Home() {
     return (
-        <>
-            <img 
-                src={ Avatar }
-                alt="avatar"
-                className="rounded-full w-64 h-64 border border-black bg-black"
-            />
-            <h1 className="">Azeddine Elmakkaoui</h1>
-        </>
+        <div className="flex justify-center items-center h-[calc(100vh-60px)]">
+            <div className="wrap">
+                <img 
+                    src={Avatar}
+                    alt="avatar"
+                    className="rounded-full w-64 h-64 border border-black bg-black"
+                />
+                <h1 className="font-bold text-2xl">Azeddine Elmakkaoui</h1>
+            </div>
+        </div>
     );
 }
